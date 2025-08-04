34a05c75466c57549923be10a78f45414d12b392
Vernieri <jvmendes.ti@gmail.com>
2025-08-04 15:36:41 -0300

    Adding JWT Stuff

---
+++ mern-shop-backend/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}, {
+  timestamps: true,
+});
+
+// Hash de senha antes de salvar
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Método para comparar senha
+userSchema.methods.matchPassword = function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model('User', userSchema);
